4eba7e2aa5323ca19a7ae370c6a12c165b454dd8
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-03-07 15:11:35 +0530

    correction in experience number

--- src/components/ExperienceNumber.jsx
+++ src/components/ExperienceNumber.jsx
@@ -7,7 +7,7 @@ export default function ExperienceNumber() {
            +02 <br/> <span className="text-gray-600 text-sm 2xl:text-3xl">Years of experience</span>
           </h2>
           <h2 className="text-3xl md:text-4xl 2xl:text-6xl font-bold text-white mx-4">
-           +46 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">Projects completed</span>
+           +38 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">Projects completed</span>
           </h2>
           <h2 className="text-3xl md:text-4xl 2xl:text-6xl font-bold text-white mx-4">
            +20 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">worldwide clients</span>
