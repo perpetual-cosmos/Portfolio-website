b2a6a332cf8edd2acbd7a147b725b7c5659b6d55
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-09-10 00:41:42 +0530

    change content in self profile

--- src/components/ExperienceNumber.jsx
+++ src/components/ExperienceNumber.jsx
@@ -4,13 +4,13 @@ export default function ExperienceNumber() {
   return (
     <div className='flex flex-col sm:flex-row justify-center md:justify-start gap-4'>
           <h2 className="text-3xl md:text-4xl 2xl:text-6xl font-bold text-white mx-4">
-           +02 <br/> <span className="text-gray-600 text-sm 2xl:text-3xl">Years of experience</span>
+           +01 <br/> <span className="text-gray-600 text-sm 2xl:text-3xl">Years of experience</span>
           </h2>
           <h2 className="text-3xl md:text-4xl 2xl:text-6xl font-bold text-white mx-4">
-           +38 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">Projects completed</span>
+           +48 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">Projects and Pr completed</span>
           </h2>
           <h2 className="text-3xl md:text-4xl 2xl:text-6xl font-bold text-white mx-4">
-           +20 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">worldwide clients</span>
+           +10 <br/><span className="text-gray-600 text-sm 2xl:text-3xl">worldwide clients</span>
           </h2>
       </div>
   )
